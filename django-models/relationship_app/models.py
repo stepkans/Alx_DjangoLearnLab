65b5be08f5fb00a56672c1b0c85f86d6ea66fee5
Stephen Suyanka <stepkans@gmail.com>
2024-08-15 14:21:58 +0300

    Move rlationship app

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -4,16 +4,28 @@ from django.db import models
 class Author(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+
 
 class Book(models.Model):
     title = models.CharField(max_length=100)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return f'{self.name} by {self.author}'
+
 class Library(models.Model):
     name = models.CharField(max_length=100)
     books = models.ManyToManyField(Book)
 
+    def __str__(self):
+        return self.name
+
 class Librarian(models.Model):
     name = models.CharField(max_length=100)
     library = models.OneToOneField(Library, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
 
