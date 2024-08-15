649bbd93f823bab17439562e18dc10a3b4bf2362
Stephen Suyanka <stepkans@gmail.com>
2024-08-15 20:42:07 +0300

    Add views in relationship app

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import Author, Book, Library, Librarian
 
 # Create your views here.
+def books_list(request):
+    book_list = Book.objects.all()
+
+    context = {
+        'book_list': book_list,
+    }
+    return render(request, 'registration_app/list_books.html', context)
+
+class BooksInLibrary(ListView):
+    model = Library
+    model = Book
+
+    template_name = 'relationship_app/books_in_library.html'
+    
