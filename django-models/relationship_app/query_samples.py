68411eea0414e4f05690640481ef2a3c0d4fe76d
Stephen Suyanka <stepkans@gmail.com>
2024-08-15 18:20:47 +0300

    Add query samples script

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -1,31 +1,21 @@
 from .models import Author, Book, Library, Librarian
 from django.shortcuts import render
 
-# Prepare a Python script query_samples.py in the relationship_app directory. 
-# This script should contain the query for each of the following of relationship:
-# Query all books by a specific author.
-# List all books in a library.
-# Retrieve the librarian for a library.
-
-
-def books_by_author(request, author):
-    books_by_author = Book.objects.filter(author=author)
-    context = {
-        'books_by_author': books_by_author ,
-    }
-    return render (request, context, 'relationship_app/books_by_author.html')
-def all_books(request):
-    all_books = Book.objects.all()
-    context = {
-        "all_books" : all_books,
-    }
-    return render (request, context, 'relationship_app/all_books.html')
-
-def librarians(request):
-    librarian = Library.objects.get(librarian)
+def books_by_author(author_name):
+    author = Author.objects.get(name=author_name)
+    books = Book.objects.filter(author=author)
+    return books
     
-    context = {
-        "librarian" : librarian,
-    }
-    return render (request, context, 'relationship_app/librarian.html')
+
+def all_books_in_a_library(library_name):
+    library = Library.objects.get(name=library_name)
+    books = library.books.all()
+    books.all()
+    return books
+    
+
+def library_librarian(library_name):
+    library = Library.objects.get(name=library_name)
+    librarian = Librarian.objects.get(library=library)
+    return librarian
 
