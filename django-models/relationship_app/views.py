d4af572c1605f8fdcb3cd827c9a033b5dcefcc1d
Stephen Suyanka <stepkans@gmail.com>
2024-08-15 22:09:43 +0300

    Add auth urls and register view

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from .models import Library
 from django.views.generic.detail import DetailView
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import redirect
 
-# Create your views here.
+# Create your views here. .
 def books_list(request):
     book_list = Book.objects.all()
 
@@ -14,4 +16,16 @@ def books_list(request):
 class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
-    
+
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')    
+    else:
+        form = UserCreationForm() 
+    context = {
+        'form': form,
+    }    
+    return render(request, 'relationship_app/library_detail.html', context)
