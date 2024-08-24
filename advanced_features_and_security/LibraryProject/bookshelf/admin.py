adca32c537080bd70bf5b85afab22becce2bd034
Stephen Suyanka <stepkans@gmail.com>
2024-08-24 09:31:04 +0300

    Spelling check on CustomUserAdmin at admin.py

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -4,11 +4,12 @@ from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
 # Register your models here.
 
-class CustomUserManager(UserAdmin):
+class CustomUserAdmin(UserAdmin):
     model = CustomUser
     list_display = ('email', 'username' ,'date_of_birth','is_staff')
 
-admin.site.register(CustomUser,CustomUserManager)
+
+admin.site.register(CustomUser, CustomUserAdmin)
 
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'publication_year')
