8d155b3ec3d614c8482cd9870da58c9de73c3385
Stephen Suyanka <stepkans@gmail.com>
2024-08-24 10:33:42 +0300

    Import Example form in views

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import permission_required
 from .models import Book
-from .forms import BookForm, ExampleForm
+from .forms import ExampleForm, BookForm
 
 
 # Create your views here.
