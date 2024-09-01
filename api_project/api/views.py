bc23b845651075662876647a94c15335a9412b59
Stephen Suyanka <stepkans@gmail.com>
2024-09-01 09:38:55 +0300

    Add Authentication in API

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .serializers import BookSerializer
 from rest_framework import viewsets
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 from .models import Book
 # Create your views here.
 
@@ -12,5 +13,6 @@ class BookListAPIView(generics.ListAPIView):
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer  
+    permission_classes = IsAuthenticated
 
 
