60bbd0d4fc94153aecf2e51f0f2f826dd5e618b2
Stephen Suyanka <stepkans@gmail.com>
2024-09-08 20:51:21 +0300

    Add docs to models.py

--- advanced-api-project/api/serializers.py
+++ advanced-api-project/api/serializers.py
@@ -3,11 +3,19 @@ from .models import Author, Book
 import datetime
 
 class AuthorSerializer(serializers.ModelSerializer):
+    """
+        The Author model represents a book author.
+        It contains just a 'name' field for the author's name.
+    """
     class Meta:
         model = Author
         fields = ('name',)
 
 class BookSerializer(serializers.ModelSerializer):
+    """
+        The Book model represents a book.
+        It has these fields 'title', 'publication_year', and a foreign key to the Author model.
+    """
     class Meta:
         model = Book
         fields = "__all__"
