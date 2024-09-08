1153163bbf02c159dcf6d63c1b7e5c5e7abd4b18
Stephen Suyanka <stepkans@gmail.com>
2024-09-08 22:44:56 +0300

    Add proper testing using response.data

--- advanced-api-project/api/test_views.py
+++ advanced-api-project/api/test_views.py
@@ -23,31 +23,44 @@ class BookAPITests(APITestCase):
             author=self.author
         )
     def test_create_book(self):
-        url = reverse('book-list')
         data = {
             'title': 'New Book',
             'publication_year': 2022,
             'author': self.author.id
         }
-        response = self.client.post(url, data, format='json')
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        response = self.client.post(reverse('book-list'), data, format='json')
+        
+        self.assertEqual(response.status_code, 201)
         self.assertEqual(Book.objects.count(), 2)
-        self.assertEqual(Book.objects.latest('id').title, 'New Book')
+        self.assertEqual(response.data['title'], 'New Book')
         
     def test_update_book(self):
-        url = reverse('book-detail', args=[self.book.id])
-        data = {
-            'title': 'Updated Book',
+        # Prepare data to update the book
+        update_data = {
+            'title': 'Updated Book Title',
             'publication_year': 2021,
-            'author': self.author.id
-        }
-        response = self.client.put(url, data, format='json')
+            'author': self.author.id  
+        response = self.client.put(reverse('book-detail', args=[self.book.id]), update_data, format='json')
+
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.book.refresh_from_db()
-        self.assertEqual(self.book.title, 'Updated Book')
+        
+        # Ensure the response contains updated data
+        self.assertEqual(response.data['title'], 'Updated Book Title')
+        self.assertEqual(response.data['publication_year'], 2021)
+        
+        # Ensure the book in the database has been updated
+        self.book.refresh_from_db()  # Refresh the book instance from the database
+        self.assertEqual(self.book.title, 'Updated Book Title')
+        self.assertEqual(self.book.publication_year, 2021)
           
     def test_delete_book(self):
-        url = reverse('book-detail', args=[self.book.id])
-        response = self.client.delete(url)
+        book_id = self.book.id
+        response = self.client.delete(reverse('book-detail', args=[book_id]))
+        
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
-        self.assertEqual(Book.objects.count(), 0)      
+        self.assertEqual(Book.objects.count(), 0)
+    
+    def test_for_unauthenticated_user(self):
+        self.client.logout()
+        response = self.client.post(reverse('book-list'), {})
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)          
