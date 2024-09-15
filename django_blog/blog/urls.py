0fe6fdf2424f10c0e61379db67f75ff1a216e3a6
Stephen Suyanka <stepkans@gmail.com>
2024-09-15 16:11:00 +0300

    Change edit to update in update url

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     path('post/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
     
     path('post/new/', views.PostCreateView.as_view(), name='post-create'),
-    path('post/<int:pk>/edit/', views.PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/update/', views.PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post-delete'),
     
 ]
