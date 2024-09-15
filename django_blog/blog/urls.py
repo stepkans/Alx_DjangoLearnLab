fe8b7a135ceee1312c4c5f4be3155c0ef96193fc
Stephen Suyanka <stepkans@gmail.com>
2024-09-15 20:50:21 +0300

    Add comments url

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -14,8 +14,9 @@ urlpatterns = [
     path('post/<int:pk>/update/', views.PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post-delete'),
     
-    path('post/<int:pk>/comment/new/', views.CommentCreateView.as_view(), name='comment-create'),
+    path('post/<int:pk>/comments/new/', views.CommentCreateView.as_view(), name='comment-create'),
     path('comment/<int:pk>/update/', views.CommentUpdateView.as_view(), name='comment-update'),
     path('comment/<int:pk>/delete/', views.CommentDeleteView.as_view(), name='comment-delete'),
+    
     
 ]
