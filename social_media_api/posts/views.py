8444a88589268e57bb3b943c03a8644e8b379fde
Stephen Suyanka <stepkans@gmail.com>
2024-09-22 19:28:50 +0300

    Add feed views

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -3,6 +3,10 @@ from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
 from rest_framework.permissions import BasePermission, IsAuthenticatedOrReadOnly
 from rest_framework import filters
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from .models import Post
+from .serializers import PostSerializer
 
 class IsAuthorOrReadOnly(BasePermission):
     def has_object_permission(self, request, view, obj):
@@ -27,4 +31,15 @@ class CommentViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
+        
+
+
+class FeedView(generics.ListAPIView):
+    serializer_class = PostSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Post.objects.filter(author__in=user.following.all()).order_by('-created_at')
+        
 
