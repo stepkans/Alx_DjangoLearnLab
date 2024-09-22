d5e0fafc4f0533d6728374a599d4caf228c66f76
Stephen Suyanka <stepkans@gmail.com>
2024-09-22 09:44:01 +0300

    add api views to register

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from django.contrib.auth import get_user_model
+from .serializers import UserSerializer
+from rest_framework import status
 
-# Create your views here.
+User = get_user_model()
+
+@api_view(['POST'])
+def register(request):
+    serializer = UserSerializer(data=request.data)
+    if serializer.is_valid():
+        user = serializer.save()
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({'token': token.key}, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['POST'])
+def login(request):
+    # Add login logic here (can use Django's built-in authentication)
+    pass
