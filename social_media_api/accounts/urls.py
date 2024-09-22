657414405f82e6cc7824a3bdc124eed2a522022c
Stephen Suyanka <stepkans@gmail.com>
2024-09-22 19:11:35 +0300

    Add urls to accounts

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from .views import register, login
+from .views import FollowUserView, UnfollowUserView
 
 urlpatterns = [
     path('register/', register, name='register'),
     path('login/', login, name='login'),
+    path('users/<int:user_id>/follow/', FollowUserView.as_view(), name='follow-user'),
+    path('users/<int:user_id>/unfollow/', UnfollowUserView.as_view(), name='unfollow-user'),
 ]
