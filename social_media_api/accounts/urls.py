63ffe98944b83d3e0fde5f4f5dad852321638e58
Stephen Suyanka <stepkans@gmail.com>
2024-09-22 19:54:03 +0300

    Add post feed url

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -5,6 +5,6 @@ from .views import FollowUserView, UnfollowUserView
 urlpatterns = [
     path('register/', register, name='register'),
     path('login/', login, name='login'),
-    path('users/<int:user_id>/follow/', FollowUserView.as_view(), name='follow-user'),
-    path('users/<int:user_id>/unfollow/', UnfollowUserView.as_view(), name='unfollow-user'),
+    path('follow/<int:user_id>/', FollowUserView.as_view(), name='follow-user'),
+    path('unfollow/<int:user_id>/', UnfollowUserView.as_view(), name='unfollow-user'),
 ]
--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,9 +1,15 @@
 from rest_framework.routers import DefaultRouter
 from django.urls import path
 from .views import PostViewSet, CommentViewSet
+from .views import FeedView
 
 router = DefaultRouter()
 router.register(r'posts', PostViewSet)
 router.register(r'comments', CommentViewSet)
 
 urlpatterns = router.urls
+
+
+urlpatterns = [
+    path('feed/', FeedView.as_view(), name='feed'),
+]
